9d73b7fa21932c507096f0c3094ab1550586775f
[shaigak] <shaigak@code.edu.az>
2023-05-23 00:01:14 +0300

    newproje

---
+++ demo-app/src/App.js
@@ -0,0 +1,217 @@
+import logo from './assets/img/saiq2.jpg';
+import './App.css';
+
+
+function App() {
+  return (
+
+   
+
+    <div class="Resume d-flex">
+
+      <div class="about ">
+       
+        <div class="container">
+        <h3 class="contact">Contact : </h3>
+        <hr></hr>
+        
+        <h1>Email : Shaigak@code.edu.az</h1>
+        <hr></hr>
+        <h1>Address : 7 Microdistrict</h1>
+        <hr></hr>
+        <h1>Phone  :  +994 (50)-800-99-88</h1>
+
+        <hr></hr>
+
+       
+
+        </div>
+
+        <div class="workskil">
+        <div class="container">
+        <h3 class="skl">Skills : </h3>
+        <h1>Microsoft Excell , PowerPoint </h1>
+        <h1>Microsoft World, C#</h1>
+        <h1>ASP.NET Core , WEB API</h1>
+        <h1> SQL , HTML ,  CSS ,  GIT</h1>
+        <h1> Software Architecture ,</h1>
+        <h1> Algorithms, </h1>
+        <h1> Solid Design Principles,</h1>
+        <h1> Sony Vegas Pro,</h1>
+        <h1> Adobe Premier</h1>
+        <hr></hr>
+
+        </div>
+        </div>
+
+        
+        <div class="container">
+        
+        <div class="language">
+          
+        <h3 className='lang'>Language : </h3>
+        <hr></hr>
+        <h1>Azerbaijan : Native</h1>
+        <hr></hr>
+        <h1>English : Intermediate</h1>
+        <hr></hr>
+        <h1>Russian : Limited working proficiency</h1>
+        <hr></hr>
+        <h1> Turkish : Intermediate</h1>
+        </div>
+        <hr></hr>
+       
+
+        </div>
+
+
+
+
+       
+        
+
+      </div>
+
+      <div class="skills">
+      <div class="container">
+        
+        <div class="education">
+        <h3 class="educ" >Education : </h3>
+        <h1> ● Secondary School No. 244</h1>🏴󠁧󠁢󠁥󠁮󠁧󠁿 (2004-2015) 
+        <hr></hr>
+       
+        <h1> ● Baku State University Geological Engineering</h1> 🏴󠁧󠁢󠁥󠁮󠁧󠁿(2015-2019)
+        <hr></hr>
+       
+        <h1> ● Code Academy Full Stack Developer</h1> 🏴󠁧󠁢󠁥󠁮󠁧󠁿(2022-2023)
+        <hr></hr>
+      
+
+
+        <div class="experience">
+        <h3 class="exp" >Experience : </h3>
+        <h1> ● Elektra Enerji Club / Bar manager </h1> 🏴󠁧󠁢󠁥󠁮󠁧󠁿 (2016-2018)
+        <hr></hr>
+     
+        <h1> ● Top Kimi Restaurant / Bar manager  </h1> 🏴󠁧󠁢󠁥󠁮󠁧󠁿(2018-2019)
+        <hr></hr>
+      
+        <h1> ● StarWay Woice  / Sound Director</h1> 🏴󠁧󠁢󠁥󠁮󠁧󠁿(2019-2020)
+        <hr></hr>
+     
+        <h1> ● Tour agent in Moscow  / Registration</h1> 🏴󠁧󠁢󠁥󠁮󠁧󠁿(2021-2022)
+        <hr></hr>
+       
+        <h1> ● Caspicar Rent A Car  / Director</h1> 🏴󠁧󠁢󠁥󠁮󠁧󠁿(9 mounth)
+        
+        
+
+
+
+
+
+        </div>
+    
+        </div>
+
+        
+      
+        </div>
+
+
+      </div>
+
+      
+
+
+      <div class="workexp">
+        <div class="container">
+        <div class="name">
+              <h1 class="namemy">Shaig Khazimov </h1>
+              <h1 class="software">Software Developer</h1>
+              <img className='myphoto' src={logo}></img>
+              
+             </div>
+           <h3 class="exp" > ✦Software Developer </h3>
+           <hr></hr>
+           <h1>-Developing and supporting server Api </h1>
+
+           <h1>-(Asp.net core) </h1>
+
+
+            <h1>-Worked with Api (Javascript)</h1>
+
+
+             <h1>-Worked with Github</h1>
+
+        </div>
+
+
+
+        <div class="workexp">
+          <div class="container">
+           <h3 class="exp" >✦ Elektra ,  Enerji Club , Top kimi Restaurant </h3>
+           <hr></hr>
+           <h1>-Working with diverse personalities both on the staff and patrons </h1>
+
+           <h1>-Managing the business aspects of the bar, such as keeping a current liquor license, negotiating supplier contracts, taking inventory and reordering supplies, managing budgets, and setting goals. </h1>
+
+            <h1>-Creating effective schedules and quickly resolving conflicts to ensure that bar is well staffed during peak hours. </h1>
+
+            <h1>-Ensuring licenses are updated and in line with current legislation.</h1>
+
+        </div>
+
+        <div class="container">
+           <h3 class="exp" > ✦Tour Agencies </h3>
+           <hr></hr>
+           <h1>-Plan and sell transportations, accommodations, insurance and other travel services</h1>
+
+           <h1>-Cooperate with clients to determine their needs and advise them appropriate </h1>
+
+            <h1>-Provide relevant information, brochures and publications (guides, local customs, maps, regulations, events etc) to travelers </h1>
+
+        </div>
+
+        <div class="container">
+           <h3 class="exp" > ✦Caspi Car Rent a Car</h3>
+           <hr></hr>
+           <h1>-Processing paperwork such as collecting signatures and scanning IDs to ensure compliance with local laws</h1>
+
+           <h1>-Communicating with customers to answer questions and address concerns about their reservations or rental vehicles</h1>
+
+
+        </div>
+
+
+        <div class="container">
+           <h3 class="exp" > ✦StarWay Woice</h3>
+           <hr></hr>
+           <h1>-Safely setting up, repairing, servicing, and replacing equipment when required.</h1>
+
+           <h1>-Manipulating and enhancing live and recorded audio.</h1>
+
+           <h1>-Understanding briefs and making helpful suggestions if possible and appropriate.</h1>
+
+
+        </div>
+        
+      </div>
+      
+      </div>
+
+
+      
+
+
+
+
+      
+      
+    </div>
+    
+   
+  );
+}
+
+export default App;
